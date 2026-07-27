0fb28c00dcaa16646c54c65c395b2fa1a8b36b32
mijastijepic <mijastijepic@gmail.com>
2026-07-27 17:37:11 +0200

    funkcija, ki pridobi seznam igralcev za eno ekipo

--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -24,3 +24,43 @@ def pridobi_soup(url):
         return None
  
     return BeautifulSoup(odgovor.text, "html.parser")
+
+
+
+# -------------------------------------------------------------------
+# funkcija, ki pridobi seznam igralcev za eno ekipo
+# -------------------------------------------------------------------
+
+def pridobi_seznam_igralcev(spol, id_ekipe):
+
+    url = KONST.URL_IGRALCI_EKIPE.format(spol=spol, id_ekipe=id_ekipe)
+    
+    soup = pridobi_soup(url)
+    if soup is None:
+        return []
+ 
+    igralci = []
+ 
+    for vrstica in soup.find_all("tr"):
+        celice = vrstica.find_all("td")
+        if len(celice) != 3:
+            continue  # to ni vrstica z igralcem (npr. glava tabele)
+ 
+        celica_imena = celice[1]
+        povezava = celica_imena.find("a")
+        if povezava is None or not povezava.get("href"):
+            continue  # npr. trener nima povezave na svojo stran, zato ga preskočimo
+ 
+        ujemanje = re.search(r"(\d+)/?$", povezava["href"])
+        if ujemanje is None:
+            continue
+ 
+        igralci.append({
+            "id_igralca": ujemanje.group(1),
+            "st_dresa": celice[0].get_text(strip=True),
+            "ime_na_seznamu": celica_imena.get_text(strip=True),
+            "pozicija_kratica": celice[2].get_text(strip=True),
+        })
+        
+ 
+    return igralci
