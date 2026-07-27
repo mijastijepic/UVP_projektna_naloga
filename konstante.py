38d1cd0c4d0f4b5b21ea3d1a882cff1fe2e8eb64
mijastijepic <mijastijepic@gmail.com>
2026-07-27 16:32:07 +0200

    dodajane ID za moške in ženske ekipe

--- konstante.py
+++ konstante.py
@@ -11,6 +11,7 @@ GLAVE = {
 }
 
 
+
 # -----------------------------------------------------------------------
 # OSNOVNI NASLOVI (URL-ji) STRANI VOLLEYBALLWORLD.COM
 # -----------------------------------------------------------------------
@@ -24,3 +25,51 @@ URL_IGRALEC = (
     "https://en.volleyballworld.com/volleyball/competitions/"
     "volleyball-nations-league/players/{id_igralca}"
 )
+
+
+
+# -----------------------------------------------------------------------
+# ID-JI EKIP ZA VNL 2026
+# -----------------------------------------------------------------------
+
+EKIPE_MOSKI = {
+    8599: "Argentina",
+    8600: "Belgium",
+    8601: "Brazil",
+    8602: "Bulgaria",
+    8603: "Canada",
+    8604: "China",
+    8605: "Cuba",
+    8606: "France",
+    8607: "Germany",
+    8608: "Iran",
+    8609: "Italy",
+    8610: "Japan",
+    8611: "Poland",
+    8612: "Slovenia",
+    8613: "Serbia",
+    8614: "Turkiye",
+    8615: "Ukraine",
+    8616: "USA",
+}
+ 
+EKIPE_ZENSKE = {
+    8617: "Belgium",
+    8618: "Brazil",
+    8619: "Bulgaria",
+    8620: "Canada",
+    8621: "China",
+    8622: "Czechia",
+    8623: "Dominican Republic",
+    8624: "France",
+    8625: "Germany",
+    8626: "Italy",
+    8627: "Japan",
+    8628: "Netherlands",
+    8629: "Poland",
+    8630: "Serbia",
+    8631: "Thailand",
+    8632: "Turkiye",
+    8633: "Ukraine",
+    8634: "USA",
+}
